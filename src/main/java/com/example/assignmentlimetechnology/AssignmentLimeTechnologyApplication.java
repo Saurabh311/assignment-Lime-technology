25a8ce6d9fa8f12aea9df3c16d17eb29b4618738
Saurabh Chauhan <saurabh.chauhan@2mnordic.com>
2022-06-10 14:54:53 +0200

    First commit

---
+++ src/main/java/com/example/assignmentlimetechnology/AssignmentLimeTechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignmentlimetechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentLimeTechnologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentLimeTechnologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assignmentlimetechnology/AssignmentLimeTechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignmentlimetechnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentLimeTechnologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
